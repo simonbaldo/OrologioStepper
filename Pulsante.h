60cefb6b6f326ad8232e9fbab77872c3d08463ed
Simone Baldo <120671869+simonbaldo@users.noreply.github.com>
2023-06-05 09:37:41 +0200

    Create Pulsante.h

---
+++ Pulsante.h
@@ -0,0 +1,26 @@
+#ifndef __PULSANTE_H_
+#define __PULSANTE_H_
+
+#include <Arduino.h>
+
+#define debounceDelay 150
+
+class Pulsante{
+
+    private:
+        uint32_t tPressed;
+        uint32_t tReleased;
+        uint32_t secPress;
+        boolean pulPressed;
+        boolean pulReleased;
+        uint8_t pinPulsante;
+        boolean preState;
+        boolean curState;
+
+    public:
+       Pulsante(uint8_t pinPulsante, unsigned long secPress);
+       bool pressed();
+       uint8_t released();
+};
+
+#endif
